4e5481888717202a3740d375e83cac4a340d445f
Jozef Cajkovic <jozef.cajkovic@gmail.com>
2023-06-15 15:11:39 +0200

    ci(src): PIXEL-10 add vpc_endpoints_list and vpc_report (#5)

--- src/__main__.py
+++ src/__main__.py
@@ -1,3 +1,9 @@
-import alb_processing
+# import alb_processing
+import vpc_endpoints_list
+# import vpc_report
   
-alb_processing.process_loadbalancers_in_accounts()
+# alb_processing.process_loadbalancers_in_accounts()
+vpc_endpoints_list.process_vpc_endpoints_in_accounts()
+# vpc_report.process_vpcs_in_accounts()
+
+# vpc-74b15410
